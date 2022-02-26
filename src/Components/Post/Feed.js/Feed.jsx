24422dc97824c8f09f83f318af09952a5f499d41
Yonela Johannes <69614136+Yonela-Johannes@users.noreply.github.com>
2022-02-26 14:39:01 +0200

    Feed styling

--- src/Components/Post/Feed.js/Feed.jsx
+++ src/Components/Post/Feed.js/Feed.jsx
@@ -1,14 +1,19 @@
 import './Feed.css'
 import Post from '../Post';
-
+import { RiKakaoTalkLine} from "react-icons/ri";
+import Input from '../Feed.js/NewFeed/Input'
 function Feed( {user, admin} ) {
 
     // if(loading) return <Spinner message="We are refreshing your data" />
     return (
         <div className="text-white max-w-2xl sm:ml-[73px] xl:ml-[370px]">
             <div className='text-[gray] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-gray border-b border-gray-700'>
-                <Post />
+                <h2 className='head'>Descussion</h2>
+                <div className='w-9 h-9 flex items-center justify-center xl:px-0 ml-auto'>
+                    <RiKakaoTalkLine className='icon'/>
+                </div>
             </div>
+            <Input user={user} />
         </div>
     );
 }
--- src/Components/Post/Feed.js/NewFeed/Input.jsx
+++ src/Components/Post/Feed.js/NewFeed/Input.jsx
@@ -5,6 +5,7 @@ import { useState, useRef } from 'react';
 import { MdMonochromePhotos } from "react-icons/md";
 import { HiPhotograph } from "react-icons/hi";
 import { BsEmojiDizzyFill } from "react-icons/bs";
+import { BiMessageSquareAdd } from "react-icons/bi";
 // import { IconName } from "react-icons/ai";
 
 export default function Input({user, admin }) {
@@ -22,59 +23,62 @@ export default function Input({user, admin }) {
     let emoji = String.fromCodePoint(...codesArray);
     setInput(input + emoji);
   };
-  
 
   const addImageTopPost = () => {
 
   }
 
   return (
-    <div className='inputContainer'>
+    <div className={`border-b border-gray-700 p-3 flex-space-x3 overflow-y-scroll`}>
       <div className="userDetails">
         <p className='userName'>{user?.userName}</p>
-          <img src={user?.image} alt='' className='adminImage' />
+          <img src={user?.image} alt='' className='h-11 w-11 rounded-full cursor-pointer' />
       </div>
-            <div className='emojisWrapper'>
-              <textarea  rows='2' className='textInput' onChange={(e) => setInput(e.target.value)}/>
-                <div className='IconsEmojiContainer'>
-                  <div className='iconsContainer'>
-                    <img src={selectedFile} alt="" className='selectedImage'></img>
+        <div className='w-full divide-y divide-gray-700'>
+          <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
+            <textarea placeholder="What's is happening?" rows='2' className='bg-transparent outline-none text-gray placeholder-gray-500 txt-lg tracking-wide w-full min-h-[50px]' value={input} onChange={(e) => setInput(e.target.value)}/>
+            {selectedFile && ( 
+              <div className='relative'>
+                <div className='absolute w-8 h-8 bg-[pink] hover:[bg-gray] bg-opacity-75 rounded-full flex items-center 
+                  justify-center top-1 left-1 cursor-pointer' onClick={() => selectedFile(null)}><BiMessageSquareAdd /></div>
+                  <img src={selectedFile} alt=" select " className='rounded-2xl max-h-80 0bject-contain' />
+              </div>
+            )}               
+            <div className='flex item-center justify-between pt-2.5'>
+                <div className='flex items-center'>
+                  <div className='feedIcon' onClick={() => filePickerRef.current.click()}>
+                    <HiPhotograph className='h-[22px] text-[#aaaaaa]' />
+                    <input type='file' onChange={addImageTopPost} ref={filePickerRef} hidden/>
                   </div>
-                  <div className=''>
-                    <div className='inputIcon' onClick={() => filePickerRef.current.click()}>
-                      <HiPhotograph className='iconColor' />
-                      <input type='file' onChange={addImageTopPost} ref={filePickerRef} hidden/>
-                    </div>
+                  <div className='feedIcon' onClick={() => setShowEmojis(!showEmojis)}>
+                    <BsEmojiDizzyFill className='h-[22px] text-[#aaaaaa]' />
                   </div>
-                  <div className=''>
-                    <div className='inputIcon' onClick={() => setShowEmojis(!showEmojis)}>
-                      <BsEmojiDizzyFill className='iconColor' />
-                    </div>
-                  </div>
-                <button disabled={!input.trim() && selectedFile} className="postButton" 
-              // onClick={sendPost}
-            >Post News</button>
+                    {showEmojis && (
+                      <Picker 
+                      title="Pick emoji"
+                        onSelect={addEmoji} className="emojiPicker"
+                        style={{
+                          position: "absolute",
+                          marginTop: '20px',
+                          marginLeft: -40,
+                          maxWidth: '500px',
+                          borderRadius: '10px',
+                        }}
+                        skintones = {{
+                          1: 'Medium-Light Skin Tone',
+                          2: 'Medium Skin Tone',
+                          3: 'Medium-Dark Skin Tone',
+                          4: 'Dark Skin Tone',
+                        }}
+                        theme='light'
+                      />
+                    )}
                 </div>
+              <button  className="postButton" // onClick={sendPost}
+              >Post News</button>
             </div>
-            {showEmojis && (
-              <Picker 
-              title="Pick emoji"
-                onSelect={addEmoji} className="emojiPicker"
-                style={{
-                  position: "absolute",
-                  marginTop: '90px',
-                  maxWidth: '500px',
-                  borderRadius: '10px',
-                }}
-                skintones = {{
-                  1: 'Medium-Light Skin Tone',
-                  2: 'Medium Skin Tone',
-                  3: 'Medium-Dark Skin Tone',
-                  4: 'Dark Skin Tone',
-                }}
-                theme='dark'
-              />
-            )}
+          </div>
+        </div>              
     </div>
   )
 }
